b7b329b178c543d327098a301b6cb9f40927beca
kazuki watarai <kazukiwatarai@mbp.local>
2020-01-26 16:37:09 +0900

    ポートフォリオ完成

--- js/main.js
+++ js/main.js
@@ -1,63 +1,78 @@
-// Created for an Articles on:
-// https://www.html5andbeyond.com/bubbling-text-effect-no-canvas-required/
+// scroll header
+$(window).scroll(function() {
 
-jQuery(document).ready(function ($)
-{
-
-  // Define a blank array for the effect positions. This will be populated based on width of the title.
-  var bArray = [];
-  // Define a size array, this will be used to vary bubble sizes
-  var sArray = [4, 6, 8, 10];
-
-  // Push the header width values to bArray
-  for (var i = 0; i < $('.bubbles').width(); i++)
-  {
-    bArray.push(i);
-  }
-
-  // Function to select random array element
-  // Used within the setInterval a few times
-  function randomValue(arr)
-  {
-    return arr[Math.floor(Math.random() * arr.length)];
-  }
-
-  // setInterval function used to create new bubble every 350 milliseconds
-  setInterval(function ()
-  {
-
-    // Get a random size, defined as variable so it can be used for both width and height
-    var size = randomValue(sArray);
-    // New bubble appeneded to div with it's size and left position being set inline
-    // Left value is set through getting a random value from bArray
-    $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
-
-    // Animate each bubble to the top (bottom 100%) and reduce opacity as it moves
-    // Callback function used to remove finsihed animations from the page
-    $('.individual-bubble').animate({
-      'bottom': '100%',
-      'opacity': '-=0.7'
-    }, 3000, function ()
-    {
-      $(this).remove()
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
     }
-    );
+});
+
+//header menu block none
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// scroll speed locationS
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+// bubbles　animation
+$(function() {
+    var bArray = [];
+    var sArray = [6, 8, 12, 14];
+
+    // width are bubbles
+    for (var i = 0; i < $('.bubbles').width(); i++) {
+        bArray.push(i);
+    }
+    // random Math
+    function randomValue(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
+
+    // .individual-bubbles plus css
+    setInterval(function() {
+        var size = randomValue(sArray);
+        $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+
+        $('.individual-bubble').animate({
+            'bottom': '100%',
+            'opacity': '-=0.7'
+        }, 3000, function() {
+            $(this).remove()
+        });
 
 
-  }, 350);
+    }, 350);
 
 });
 
-// 一定時間で文字を切り替え
+// word change
 const txts = $('.text');
 let txtIndex = -1;
 txts.hide()
 
-function showNextTxt()
-{
-  txtIndex++;
-  txts.eq(txtIndex % txts.length).fadeIn(1000).delay(3000).fadeOut(1000, showNextTxt);
+function showNextTxt() {
+    txtIndex++;
+    txts.eq(txtIndex % txts.length).fadeIn(1000).delay(3000).fadeOut(1000, showNextTxt);
 }
 
 showNextTxt();
-
